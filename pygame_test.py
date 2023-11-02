673ccb995ee6398d1c3a781d9600d0ad920c3ec4
Karan Kantaria <karanbkantaria@gmail.com>
2023-11-02 12:22:28 +0000

    Formatting

--- pygame_test.py
+++ pygame_test.py
@@ -8,47 +8,51 @@ from handle_enemy import enemy,  enemy_collision, Node
 pygame.init()
 # WINDOW_WIDTH = 704
 # WINDOW_HEIGHT = 320
-WINDOW_WIDTH = 1180
+
+WINDOW_WIDTH = 1180#16:9 wont work for maze size
 WINDOW_HEIGHT = 720
-#16:9^
 WINDOW=pygame.display.set_mode([WINDOW_WIDTH,WINDOW_HEIGHT])
 BLACK=(0,0,0)
 WIN_EDGE = pygame.Rect(WINDOW_WIDTH//2 - 5, 0, 10, WINDOW_HEIGHT)
 pygame.display.set_caption("TCA2")
 
+#Image for teh trap
 TRAP_IMAGE = pygame.image.load(os.path.join('Assets', 'spike_sprite.png'))
 TRAP_WIDTH = 20
 TRAP_HEIGHT = 20
 TRAP_COMP = pygame.transform.rotate(pygame.transform.scale(TRAP_IMAGE, (TRAP_WIDTH, TRAP_HEIGHT)), 0)
 
+#Image for the coin
 COIN_IMAGE = pygame.image.load(os.path.join('Assets', 'coin_clear_background.png')).convert_alpha()# linking coin images with a asset
 COIN_WIDTH = 20
 COINT_HEIGHT = 20
 COIN_COMP=pygame.transform.rotate(pygame.transform.scale(COIN_IMAGE, (COIN_WIDTH, COINT_HEIGHT)), 0)
 
+#Image for the health potion
 HEALTH_IMAGE=pygame.image.load(os.path.join('Assets', 'healing_potion.png')).convert_alpha()
 HEALTH_WIDTH=30
 HEALTH_HEIGHT=30
 HEALTH_COMP=pygame.transform.rotate(pygame.transform.scale(HEALTH_IMAGE, (HEALTH_WIDTH, HEALTH_HEIGHT)), 0)
 
-
+#background image level 1 only for now
 BACKGROUND_TEST = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'Grass_Sample.png')), (WINDOW_WIDTH, WINDOW_HEIGHT))
 
+#Image for the player
 PLAYER_IMAGE = pygame.image.load(os.path.join('Assets', 'test_sprite.png'))
 PLAYER_WIDTH = 15
 PLAYER_HEIGHT = 30
 PLAYER_COMP = pygame.transform.rotate(pygame.transform.scale(PLAYER_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT)), 0)#rotate redundant for now
 
-COIN_IMAGE = pygame.image.load(os.path.join('Assets', 'pngtree-glossy-golden-coin-icon-png-image_2898883.jpg'))# linking coin images with a asset
-player_score = 0  #Start of the game the player will have 0 ponits
+#Image for the Maze (wall only)
 MAZE_WALL=pygame.image.load(os.path.join('Assets', 'maze_wall_test.png'))
 
+#Image for the enemy
 ENEMY_IMAGE = pygame.image.load(os.path.join('Assets', 'monster2_test.png'))
 ENEMY_WIDTH = 15
 ENEMY_HEIGHT = 30
 ENEMY_COMP = pygame.transform.rotate(pygame.transform.scale(ENEMY_IMAGE, (ENEMY_WIDTH, ENEMY_HEIGHT)), 0)
 
-#Creating a class for player the main character
+#Classes go here
 class Player(pygame.sprite.Sprite):
   
   def __init__(self,width, height, speed, max_health,player_image,x,y,health,score,health_potions):
@@ -69,19 +73,44 @@ class Player(pygame.sprite.Sprite):
     pygame.sprite.Sprite.__init__(self)
     WINDOW = pygame.display.get_surface()
 
-#Updating moveing block to block
-def movement(entity,key_press):
-    player=entity
-    if move[pygame.K_a]:
-        player.move_ip(-1,0) 
-    elif move[pygame.K_d]:
-        player.move_ip(1,0) 
-    elif move[pygame.K_w]:
-        player.move_ip(0,-1)
-    elif move[pygame.K_s]:
-        player.move_ip(0,1)
+# Making Class for the trap 
+class Trap(pygame.sprite.Sprite):
+    def __init__(self, x, y, TRAP_IMAGE):
+        super().__init__()
+        self.image = pygame.transform.scale(TRAP_IMAGE, (TILE_SIZE, TILE_SIZE)) # Getting the trap images from assets
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+class Health_Potion(pygame.sprite.Sprite):
+    def __init__(self, x, y, HEALTH_POTION_IMAGE):
+        super().__init__()
+        self.image = pygame.transform.scale(HEALTH_POTION_IMAGE, (TILE_SIZE, TILE_SIZE)) # Getting the trap images from assets
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 
+# Could be done in check_collision and creating a coin
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y, COIN_IMAGE): 
+        super().__init__()
+        self.image = pygame.transform.scale(COIN_IMAGE, (TILE_SIZE, TILE_SIZE))  # Getting the image to the coin
+        self.rect = self.image.get_rect() 
+        self.rect.x = x
+        self.rect.y = y    #
+
+#botton class
+class Button():
+    def __init__(self, x, y, image):
+        self.image= image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        WINDOW.blit (self.image,(self.rect.x, self.rect.y)) 
+
+#All draw functions here
 def draw_maze(level):
     for y, row in enumerate(level):
         for x, cell in enumerate(row):
@@ -128,40 +157,19 @@ def draw_coins(coins_list):
     for coin in coins_list:
         WINDOW.blit(COIN_COMP, (coin.rect.x, coin.rect.y))
 
-def draw_fov(player_rect, level, WINDOW):
-    FOV_RADIUS = 100  # Adjust this to your preferred FOV radius
-    fov_mask = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
-    pygame.draw.circle(fov_mask, (0, 0, 0, 200), player_rect.center, FOV_RADIUS)
-    WINDOW.blit(fov_mask, (0, 0), special_flags=pygame.BLEND_RGBA_SUB)
 
 
-# Making Class for the trap 
-class Trap(pygame.sprite.Sprite):
-    def __init__(self, x, y, TRAP_IMAGE):
-        super().__init__()
-        self.image = pygame.transform.scale(TRAP_IMAGE, (TILE_SIZE, TILE_SIZE)) # Getting the trap images from assets
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-class Health_Potion(pygame.sprite.Sprite):
-    def __init__(self, x, y, HEALTH_POTION_IMAGE):
-        super().__init__()
-        self.image = pygame.transform.scale(HEALTH_POTION_IMAGE, (TILE_SIZE, TILE_SIZE)) # Getting the trap images from assets
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-
-
-# Could be done in check_collision and creating a coin
-class Coin(pygame.sprite.Sprite):
-    def __init__(self, x, y, COIN_IMAGE): 
-        super().__init__()
-        self.image = pygame.transform.scale(COIN_IMAGE, (TILE_SIZE, TILE_SIZE))  # Getting the image to the coin
-        self.rect = self.image.get_rect() 
-        self.rect.x = x
-        self.rect.y = y    # 
+#all other functions here
+def movement(entity,key_press):
+    player=entity
+    if move[pygame.K_a]:
+        player.move_ip(-1,0) 
+    elif move[pygame.K_d]:
+        player.move_ip(1,0) 
+    elif move[pygame.K_w]:
+        player.move_ip(0,-1)
+    elif move[pygame.K_s]:
+        player.move_ip(0,1)
 
 # # Function to check for collisions with the player and coin
 def coin_collision(player_init, coins_group):
@@ -170,30 +178,10 @@ def coin_collision(player_init, coins_group):
 
 
 
-
-
-   #LOAD IMAGES
-
+#LOAD IMAGES
 start_image=pygame.image.load(os.path.join('Assets', 'test_sprite.png'))
-
 #start_image('6orceshd.png')
-
-
-#botton class
-
-class Button():
-    def __init__(self, x, y, image):
-        self.image= image
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-
-    def draw(self):
-        WINDOW.blit (self.image,(self.rect.x, self.rect.y))
-
-
 start_button = Button(100, 200, start_image)
-
-
 
 
 
@@ -201,29 +189,19 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 
-   
-# On the screen it will show the score 
-font = pygame.font.Font(None, 36)
-score_text = font.render(f"score: {player_score}", True, (255,255,255)) #Putting score in a dict and changing font to white
-WINDOW.blit(score_text, (10,10)) # Size of text
+# pygame.display.update()
+# # Creating the time for the maze
+# pygame.init()
 
-pygame.display.update()
-
-# Creating the time for the maze
-pygame.init()
 start_time = time.time() # The time the player starting and with the current time
 time_limit = 3 * 60 # 60 seconds times 3 equals 3 mintues 
 
 
 
-
-black = (0, 0, 0)
-white = (255, 255, 255)
-
 level_1 = [
     "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     "x  S        xx                  x      x",
-    "x     C     xx      E          x       x",
+    "x     C     xx                 x       x",
     "x    xx                     x   x  xx  x", 
     "x    T    C     xxxxxxx  x  x      xx  x", 
     "x           x   xxxxxxx  x  x      xx  x",
@@ -236,7 +214,7 @@ level_1 = [
     "xxx  xxx            xxx  xxxxxxxxxxxxxxx",
     "x    xxx            xxx                x",
     "x   xxxxxxxxxxxxxxxxxx    C     P      x",
-    "x   xxxxxxxxxxxxxxxxxxx                x",
+    "x   xxxxxxxxxxxxxxxxxxx         E       x",
     "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 
 
@@ -275,7 +253,7 @@ level_2 = [
     "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     "x                        x           C x",
     "x                        x             x",
-    "x    xxx       T         x      xxxxxxxx",
+    "x    xxx E     T         x      xxxxxxxx",
     "x    xxxxxxxxxxxxxxxx    x      x  H   x",
     "x                   x    x      x      x",
     "x                   x    x      x      x",
@@ -286,7 +264,7 @@ level_2 = [
     "x         xxxxxxxxxxx    x     xxxxxxxxx",
     "x         xxx                  x   x S x",
     "x    x    xxx                T x   x   x",
-    "x    x          xxxxxxxxxxxxxxxx   x E x",
+    "x    x          xxxxxxxxxxxxxxxx   x   x",
     "x    x         C xxxx                  x",
     "x    xxxxxxxxxxxxxxx                   x",
     "x                                      x",
@@ -408,7 +386,7 @@ for y, row in enumerate(current_level):
         elif cell =="H":
             health_potion=Health_Potion(x * TILE_SIZE, y * TILE_SIZE, HEALTH_IMAGE)
             health_list.append(health_potion)   
-player_init = Player(PLAYER_WIDTH, PLAYER_HEIGHT, 100,50,PLAYER_IMAGE,player_x,player_y,101,5,0) #Creating a player as a object
+player_init = Player(PLAYER_WIDTH, PLAYER_HEIGHT, 100,50,PLAYER_IMAGE,player_x,player_y,100,0,0) #Creating a player as a object
 enemy_init = enemy(ENEMY_WIDTH,ENEMY_HEIGHT,ENEMY_IMAGE,enemy_x,enemy_y,maze_walls)
 
 
@@ -444,8 +422,7 @@ while loop:
     draw_health_potion(health_list)
     draw_health_potions(player_init)
     draw_score(player_init)
-    draw_maze(current_level)
-    draw_fov(player_init.rect, current_level, WINDOW) 
+    draw_maze(current_level) 
     
 
     move=pygame.key.get_pressed()
